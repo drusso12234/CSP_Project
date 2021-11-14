9443b6b0af5d94860a78ecf64dbdd3d477876a41
drusso12234 <drusso12234@gmail.com>
2021-11-14 00:15:15 -0600

    Add files via upload

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -20,7 +20,76 @@ public class SudokuSolver {
 	 * @return the solved Sudoku board
 	 */
 	public int[][] solve(int[][] board) {
-		// TODO write it;
+
 		return board;
 	}
+
+	public int[][] simpleDFS(int[][] board) {
+	int move[] = nextVar(0, 0, board);
+	if (move[0] == -1)
+		return board;
+	int curVal = 0;
+
+	while (nextVal(curVal) != -1) {
+		curVal = nextVal(curVal);
+		if (checkValid(board, move[0], move[1], curVal)) {
+			board[move[0]][move[1]] = curVal;
+		}
+	}
+
+	}
+
+	public boolean checkValid(int[][] board, int row, int col, int num) {
+
+		//Check if row is valid
+		for (int i = 0; i < board.length; i++) {
+			if (board[i][col] == num) {
+				return false;
+			}
+		}
+
+		//Check if col is valid
+		for (int i = 0; i < board.length; i++) {
+			if (board[row][i] == num) {
+				return false;
+			}
+		}
+
+		//Math to get location of box
+		int sqrt = (int)Math.sqrt(board.length);
+		int boxRow = row - row % sqrt;
+		int boxCol = col - col % sqrt;
+
+		for (int r = boxRow; r < boxRow + sqrt; r++) {
+			for (int d = boxCol; d < boxCol + sqrt; d++) {
+				if (board[r][d] == num) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public int[] nextVar(int row, int col, int[][] board) {
+		for (int i = row; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[i][j] == 0) {
+					return [i, j];
+				}
+			}
+		}
+		return [-1, -1];
+	}
+
+	public int nextVal(int curVal) {
+		if curVal == 0
+				return 1;
+		if curVal == 9
+				return -1;
+		else
+			return curVal++;
+	}
 }
+
+
