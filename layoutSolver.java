6cedca816727d4cc44ef7c56a8bbfc89ae3c4142
drusso12234 <drusso12234@gmail.com>
2021-11-15 22:55:52 -0600

    Add files via upload

--- layoutSolver.java
+++ layoutSolver.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class layoutSolver {
 
     public static void main(String[] argv) {
-        char[][] board = [['C', '', ''], ['', '', 'L'], ['', '', '']];
-        solve(board);
+        String[][] board = [['C', '', ''], ['', '', 'L'], ['', '', '']];
+        System.out.println(solve(board));
     }
 
-    ArrayList<ArrayList<Integer>> variables;
-    char[] vals = ['R', 'h', 'H', 'G'];
+    ArrayList<ArrayList<String>> variables;
+    String[] vals = ['R', 'h', 'H', 'G'];
 
 
     /**
@@ -26,15 +26,15 @@ public class layoutSolver {
      * @param board the 2d int array representing the Sudoku board. Zeros indicate unfilled cells.
      * @return the solved Sudoku board
      */
-    public int[][] solve(int[][] board) {
-
+    public int[][] solve(String[][] board) {
+        simpleDFS(board);
         return board;
     }
 
     //Basic backtracking solver
-    public boolean simpleDFS(int[][] board) {
+    public boolean simpleDFS(String[][] board) {
         int move[] = nextVar(board);
-        if (move[0] == -1)
+        if (checkLayout(board))
             return true;
 
         int curVal = 0;
@@ -42,13 +42,13 @@ public class layoutSolver {
         int row = move[0];
         int col = move[1];
 
-        while (nextVal(curVal) != -1) {
+        while (spaceCounter(board)) {
             curVal = nextVal(curVal);
             if (checkValid(board, row, col, curVal)) {
                 board[row][col] = curVal;
                 if (simpleDFS(board))
                     return true;
-                board[row][col] = 0;
+                board[row][col] = '';
             }
         }
         return false;
@@ -56,7 +56,7 @@ public class layoutSolver {
     //backtracking solver using the MRV and Degree heuristics
     public boolean degreeMRV(int[][] board) {
         int move[] = getVar(board);
-        if (move[0] == -1)
+        if (checkLayout(board))
             return true;
 
         int curVal = 0;
@@ -64,24 +64,39 @@ public class layoutSolver {
         int row = move[0];
         int col = move[1];
 
-        while (nextVal(curVal) != -1) {
-            curVal = nextVal(curVal);
+        while (spaceCounter(board)) {
+            curVal = getValFromDomain(row, col, curVal);
             if (checkValid(board, row, col, curVal)) {
                 board[row][col] = curVal;
                 variables = getVariables(board);
-                if (simpleDFS(board))
+                if (degreeMRV(board))
                     return true;
-                board[row][col] = 0;
+                board[row][col] = '';
             }
         }
         return false;
     }
 
-    public boolean checkLayout(char[][] board) {
+    public boolean spaceCounter(String[][] board) {
         counter = 0;
-        for (char[] row : board) {
-            for (char val : row) {
-                if (val != '') {
+        for (String[] row : board) {
+            for (String val : row) {
+                if (val.length == 2) {
+                    counter++
+                }
+            }
+        }
+        if (counter > 3)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean checkLayout(String[][] board) {
+        counter = 0;
+        for (String[] row : board) {
+            for (String val : row) {
+                if (val.length == 1) {
                     counter++;
                 }
             }
@@ -95,10 +110,12 @@ public class layoutSolver {
     }
 
     //Function to check if num is valid in that location
-    public boolean checkValid(char[][] board, int row, int col, char val) {
+    public boolean checkValid(String[][] board, int row, int col, String val) {
         if (board[row][col].length != 0) {
             return false;
         }
+        if (val == '  ')
+            return true;
         if (val == 'G') {
             if (checkClose(board, row, col, 'h') || checkClose(board, row, col, 'H'))
                 return false;
@@ -118,7 +135,7 @@ public class layoutSolver {
         return true;
     }
 
-    public boolean checkClose(char[][] board, int row, int col, char val) {
+    public boolean checkClose(String[][] board, int row, int col, String val) {
         if (row == 0) {
             if (col == 0) {
                 if (board[row + 1][col] == val || board[row][col + 1] == val)
@@ -171,7 +188,7 @@ public class layoutSolver {
         pos[1] = -1;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == 0) {
+                if (board[i][j] == '') {
                     pos[0] = i;
                     pos[1] = j;
                     return pos;
@@ -183,22 +200,28 @@ public class layoutSolver {
 
     //Simple function to get the next val to try for a variable.
     public int nextVal(int curVal) {
-        if (curVal == 9)
-            return -1;
-        else
-            return ++curVal;
+        if (curVal == 'G')
+            return '  ';
+        if (curVal == 'R')
+            return 'H';
+        if (curVal == 'H')
+            return 'h';
+        if (curVal == 'h')
+            return 'G';
+        if (curVal == '')
+            return 'R';
     }
 
     //Function to build a list of variables and their domains for use in heuristic calulation
-    public ArrayList<ArrayList<Integer>> getVariables(int[][] board) {
-        ArrayList<ArrayList<Integer>> domains = new ArrayList<>();
-        ArrayList<Integer> fixed = new ArrayList<>();
+    public ArrayList<ArrayList<String>> getVariables(String[][] board) {
+        ArrayList<ArrayList<String>> domains = new ArrayList<>();
+        ArrayList<String> fixed = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            fixed.add(0);
+            fixed.add('  ');
         }
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == 0) {
+                if (board[i][j] == '') {
                     domains.add(fillDomain(board, i, j));
                 } else {
                     domains.add(fixed);
@@ -208,21 +231,73 @@ public class layoutSolver {
         return domains;
     }
 
+    public boolean checkDomain(String[][] board, int row, int col, String val) {
+        if (board[row][col].length != 0) {
+            return false;
+        }
+        if (val == 'G') {
+            if (checkClose(board, row, col, 'h') || checkClose(board, row, col, 'H'))
+                return false;
+        }
+        if (val == 'h') {
+            if (checkClose(board, row, col, 'C') || checkClose(board, row, col, 'G'))
+                return false;
+        }
+        if (val == 'H') {
+            if (checkClose(board, row, col, 'C') || checkClose(board, row, col, 'G'))
+                return false;
+        }
+        if (val == 'R') {
+            if (!checkClose(board, row, col, 'L'))
+                return false;
+        }
+        return true;
+    }
+
     //Helper function to fill in the domain for the variables
-    public ArrayList<Integer> fillDomain(int[][] board, int row, int col) {
-        ArrayList<Integer> domain = new ArrayList<>();
-        for (int i = 1; i <= board.length; i++) {
-            if (checkValid(board, row, col, i)) {
-                domain.add(i);
+    public ArrayList<String> fillDomain(String[][] board, int row, int col) {
+        ArrayList<String> domain = new ArrayList<>();
+        ArrayList<String> valid = new ArrayList<>();
+        ArrayList<String> used = new ArrayList<>();
+        for (String[] arr : board) {
+            for (String var : arr) {
+                if (var.length == 1) {
+                    used.add(var);
+                }
+            }
+        }
+
+        for (String val : vals) {
+            if (!used.contains(val))
+                valid.add(val);
+        }
+
+        for (String val : valid) {
+            if (checkDomain(board, row, col, val)) {
+                domain.add(val);
             }
         }
         return domain;
     }
 
+    public String getValFromDomain(int row, int col, String curVal) {
+        index = (row * 3) + col;
+        ArrayList<String> valLst = variables.get(index);
+        if (curVal == '')
+            return valLst.get(0);
+        if (curVal == valLst.get(valLst.size() - 1))
+            return '  ';
+        for (int i = 0; i < valLst.size(); i++) {
+            if (valLst.get(i) == curVal)
+                return valLst.get(i + 1);
+        }
+        return '  ';
+    }
+
     //Gets length of domain lists, for use in MRV
     public ArrayList<Integer> getLength() {
         ArrayList<Integer> domainLength = new ArrayList<>();
-        for (ArrayList<Integer> lst : variables) {
+        for (ArrayList<String> lst : variables) {
             domainLength.add(lst.size());
         }
         return domainLength;
@@ -241,16 +316,16 @@ public class layoutSolver {
         for (int i = 0; i < lengths.size(); i++) {
             if (lengths.get(i) <= min) {
                 if (lengths.get(i) == min) {
-                    if (degree < getDegree(board, i / 9, i % 9)) {
-                        pos[0] = i / 9;
-                        pos[1] = i % 9;
+                    if (degree < getDegree(board, i / 3, i % 3)) {
+                        pos[0] = i / 3;
+                        pos[1] = i % 3;
                         degree = getDegree(board, pos[0], pos[1]);
                     }
                 }
                 else {
                     min = lengths.get(i);
-                    pos[0] = i / 9;
-                    pos[1] = i % 9;
+                    pos[0] = i / 3;
+                    pos[1] = i % 3;
                     degree = getDegree(board, pos[0], pos[1]);
                 }
             }
@@ -259,31 +334,5 @@ public class layoutSolver {
     }
 
     //Helper function to calculate the degree of a variable
-    public int getDegree(int[][] board, int row, int col) {
-        int degree = 0;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == 0) {
-                degree++;
-            }
-        }
 
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == 0) {
-                degree++;
-            }
-        }
-
-        int sqrt = (int)Math.sqrt(board.length);
-        int boxRow = row - row % sqrt;
-        int boxCol = col - col % sqrt;
-
-        for (int r = boxRow; r < boxRow + sqrt; r++) {
-            for (int d = boxCol; d < boxCol + sqrt; d++) {
-                if (board[r][d] == 0) {
-                    degree++;
-                }
-            }
-        }
-        return degree;
-    }
 }
